4b661b2b11b02700c2e27dc0ceb093d8e1afcc49
Rodolphe <rodolphebansard@yahoo.com>
2022-03-29 22:52:23 +0200

    add component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {JobListComponent} from "./components/job-list/job-list.component";
+import {JobDetailsComponent} from "./components/job-details/job-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'job-list' , component: JobListComponent},
+  {path: 'job/:id', component: JobDetailsComponent},
+  {path: '**', redirectTo:"job-list"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { JobListComponent } from './components/job-list/job-list.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JobListComponent,
+    JobDetailsComponent
   ],
   imports: [
     BrowserModule,
